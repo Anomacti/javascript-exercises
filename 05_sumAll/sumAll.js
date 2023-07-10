0b6defc5a991e28cde276c2ce7df4e839e76fee3
Ananias Chorozoglou <65049135+Anomacti@users.noreply.github.com>
2023-07-10 11:10:38 +0300

    Finish 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if(!Number.isInteger(start) || !Number.isInteger(end)) return "ERROR";
+    if(start < 0 || end < 0) return "ERROR";
 
+    // If we have larger number first, swap arguments
+    if(start > end) {
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+
+    let sum = 0;
+    for(let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
