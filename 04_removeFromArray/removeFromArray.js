65a2a1d2afc7e6da001352ecd9be5088c825ff44
Ananias Chorozoglou <65049135+Anomacti@users.noreply.github.com>
2023-07-10 10:53:06 +0300

    Finish 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...targets) {
+    /*
+    NOTE: JS passes objects by reference, I didn't know that
+    at the time of writing this program. I will leave the program
+    like this since I already wrote it and it works but it isn't a 
+    good solution.
+   */
 
+    for(const target of targets) {
+        for(let i = 0; i < array.length; i++) {
+            if(array[i] === target) {
+                array.splice(i, 1);
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
