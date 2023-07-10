fdac7fc21e38a2d9633d2c138239870396201410
Ananias Chorozoglou <65049135+Anomacti@users.noreply.github.com>
2023-07-10 11:58:39 +0300

    Finish 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahren) {
+  let number = ((fahren - 32) * 5) / 9;
+  return Math.round(number * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let number = (celsius * 1.8) + 32;
+  return Math.round(number * 10) / 10;
 };
 
 // Do not edit below this line
